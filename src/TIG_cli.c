0bbbafba239ff6f328c7704a617ac0fe552a723e
Your Name <you@example.com>
2025-12-21 18:05:17 +0100

    tcp frame

---
+++ src/TIG_cli.c
@@ -0,0 +1,70 @@
+#include        <sys/types.h>   /* basic system data types */
+#include        <sys/socket.h>  /* basic socket definitions */
+#include        <netinet/in.h>  /* sockaddr_in{} and other Internet defns */
+#include        <arpa/inet.h>   /* inet(3) functions */
+#include        <errno.h>
+#include        <stdio.h>
+#include        <stdlib.h>
+#include        <string.h>
+#include        <unistd.h>
+
+#define BUFF_SIZE 1024
+#define LISTEN_PORT_FTP 21
+
+int main(int argc, char **argv){
+
+    int n = 0;
+
+    int err = 0;
+
+    int sockfd = 0;
+
+    char buff[BUFF_SIZE];
+
+    struct sockaddr_in6 servaddr;
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin6_family = AF_INET6;
+    servaddr.sin6_port = htons(LISTEN_PORT_FTP);
+
+    if(argc != 2){
+        fprintf(stderr, "ERROR: usage: ./TIG_cli <Serv_addr> \n");
+        return -1;
+    }
+
+    if((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0){
+        fprintf(stderr, "socket() error: %s\n", strerror(errno));
+        return -1;
+    }
+
+    if((err = inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr)) <= 0){
+        if(err == 0){
+            fprintf(stderr, "inet_pton error for %s \n", argv[1]);
+        }
+        else{
+            fprintf(stderr, "inet_pton error for %s : %s \n", argv[1], strerror(errno));
+            return -1;
+        }
+    }
+
+    if(connect(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr)) < 0){
+        fprintf(stderr, "connect() error: %s \n", strerror(errno));
+        return -1;
+    }
+
+    while((n = read(sockfd, buff, BUFF_SIZE-1)) > 0){
+        buff[n] = 0;
+        if(fputs(buff, stdout) == EOF){
+            fprintf(stderr, "fputs() error: %s\n", strerror(errno));
+            return -1;
+        }
+    }
+
+    if (n < 0)
+		fprintf(stderr,"read error : %s\n", strerror(errno));
+
+    fflush(stdout);
+    fprintf(stderr, "\nOK\n");
+    close(sockfd);
+
+    return 0;
+}
